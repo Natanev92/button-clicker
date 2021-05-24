80ae24b70581d04c54b81832895004d43b89f274
Natanev92 <nvillasenor1@icloud.com>
2021-05-24 14:03:20 -0700

    button click

---
+++ script.js
@@ -0,0 +1,10 @@
+function button(){
+    alert("Good bye!")
+}
+function button2(){
+    alert("try again!")
+}
+var button3 =1
+for (var button3 = 1; button3 < 10; button3++){
+    console.log(buttton3)
+}
